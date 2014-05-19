37b67be8343ed24ecbd9a4a5fcfdce0176343344
joshifullop <joshifullop@users.noreply.github.com>
2014-05-19 10:02:12 -0500

    Added comentation

--- bin/barleypi_log_to.sh
+++ bin/barleypi_log_to.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# This script changes the 'LogToSession' symbolic pointer where the cron job writes its data.
+# Usage: ./barleypi_log_to.sh <session file name>
+# To stop effective logging, ./parleypi_log_to.sh /dev/null
 root_dir="/home/pi/barleypi"
 session_dir="$root_dir/sessions"
 sd_len=`expr ${#sensor_dir} + 1`
